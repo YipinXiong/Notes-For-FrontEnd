602f2cf27769b0b45b279aeca17dfe14605c75dd
Yipin <xiong1pin@gmail.com>
2019-03-17 23:52:49 +1100

    bind this for searchBar

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit (term) {
-    console.log(term);
+
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response =  await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term},
+      headers: {
+        Authorization: 'Client-ID 75f78c86f106afacc462f6f8d45b92335c379282f3d47b9bffbb3e2d54229e1e' 
+      }
+    });
+
+    this.setState( { images: response.data.results });
   }
 
   render() {  
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
